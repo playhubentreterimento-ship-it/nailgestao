6456a05d0a1f6b44dd11d765faf6717e7f2d5cb0
Rickzinho <playhubentreterimento@gmail.com>
2026-08-11 17:36:41 -0400

    Suporte Vercel Prisma Serverless

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,11 +1,20 @@
 import { PrismaClient } from "@prisma/client";
+import path from "path";
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
+const dbPath = path.join(process.cwd(), "prisma", "dev.db");
+
 export const prisma =
   globalForPrisma.prisma ??
-  new PrismaClient();
+  new PrismaClient({
+    datasources: {
+      db: {
+        url: `file:${dbPath}`,
+      },
+    },
+  });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
